8580b737544818846c88c519bafaefd1cc1f3d04
hoolek77 <szymon.kin@gmail.com>
2021-10-01 14:58:58 +0200

    config: add typescript types generation script for grapqhl

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,7 @@
 import React from 'react';
 
-import { gql, useQuery } from '@apollo/client';
-
-const CHARACTERS_QUERY = gql`
-  query CharactersQuery($page: Int!) {
-    characters(page: $page) {
-      results {
-        id
-        name
-      }
-    }
-  }
-`;
+import { Test } from 'components/Test';
 
 export const App = () => {
-  const { data, loading, error } = useQuery(CHARACTERS_QUERY);
-
-  if (loading) {
-    return <div>loading...</div>;
-  }
-
-  if (error) {
-    return <div>error</div>;
-  }
-
-  return <div>{JSON.stringify(data)}</div>;
+  return <Test />;
 };
