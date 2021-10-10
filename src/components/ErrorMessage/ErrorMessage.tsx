bf39d7c671e9eb089977b0c854169aae9c83f52a
hoolek77 <szymon.kin@gmail.com>
2021-10-10 21:58:12 +0200

    feat: add error message component

---
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -0,0 +1,15 @@
+import React, { ComponentPropsWithoutRef } from 'react';
+
+import styled from 'styled-components';
+
+type Props = ComponentPropsWithoutRef<'div'>;
+
+const Wrapper = styled.div`
+  margin: auto;
+  font-size: 24px;
+  font-weight: 700;
+`;
+
+export const ErrorMessage = ({ children, ...props }: Props) => {
+  return <Wrapper {...props}>{children}</Wrapper>;
+};
